36ea4890df1bdd0538f4b98a9f57e3d2abd242f4
eric <eric.ufjf@gmail.com>
2021-12-05 11:51:09 -0300

    Add: Novos exercícios

---
+++ NomeIdade/src/com/company/Consoantes.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Consoantes {
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        String[] consoantes = new String[6];
+        int qtdeConsoantes = 0;
+        int count = 0;
+        do {
+            System.out.println("Letra: ");
+            String letra = scan.next();
+            if (!(letra.equalsIgnoreCase("a") |
+                    letra.equalsIgnoreCase("e") |
+                    letra.equalsIgnoreCase("i") |
+                    letra.equalsIgnoreCase("o") |
+                    letra.equalsIgnoreCase("u"))){
+                consoantes[count] = letra;
+                qtdeConsoantes++;
+
+            }
+            count++;
+
+        } while(count < consoantes.length);
+        System.out.println("Quantidade de consoantes: " + qtdeConsoantes);
+        for (String consoante : consoantes){
+            if ( consoante != null ){
+                System.out.print(consoante + " ");
+            }
+
+        }
+
+    }
+}
