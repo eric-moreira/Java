4d26bb8a5637a57e6c97bf97de6e2a55a16536c6
eric <eric.ufjf@gmail.com>
2021-12-12 21:41:55 -0300

    add: ListaEncadeada e metodos

--- projetoListaEncadeada/src/main/java/lista/encadeada/ListaEncadeada.java
+++ projetoListaEncadeada/src/main/java/lista/encadeada/ListaEncadeada.java
@@ -8,10 +8,17 @@ public class ListaEncadeada<T> {
         this.referenciaEntrada = null;
     }
 
+
     public boolean isEmpty(){
         return referenciaEntrada == null ? true : false;
     }
 
+    private void validaIndex(int index){
+        if (index >= this.sizeOf()) {
+            throw new IndexOutOfBoundsException("Não existe conteudo no indice "
+                    + index + "desta lista.");
+        }
+    }
     public int sizeOf(){
         int tamanhoLista = 0;
         No<T> refAuxiliar = referenciaEntrada;
@@ -31,6 +38,8 @@ public class ListaEncadeada<T> {
 
         return tamanhoLista;
     }
+
+
     public void add(T conteudo){
         No<T> novoNo = new No<>(conteudo);
 
@@ -45,7 +54,56 @@ public class ListaEncadeada<T> {
         }
         noAuxiliar.setProximoNo(novoNo);
         return;
+    }
+
+    public T get(int index){
+
+        return this.getNo(index).getConteudo();
 
     }
 
+    public T remove(int index){
+        No<T> noPivot = this.getNo(index);
+
+        if ( index == 0 ){
+            referenciaEntrada = noPivot.getProximoNo();
+            return noPivot.getConteudo();
+        }
+
+        No<T> noAnterior = this.getNo(index -1 );
+        noAnterior.setProximoNo(noPivot.getProximoNo());
+
+        return noPivot.getConteudo();
+    }
+
+    private No<T> getNo(int index){
+
+        validaIndex(index);
+
+        No<T> noAuxiliar = referenciaEntrada;
+        No<T> noRetorno = null;
+
+
+        for (int i = 0; i < this.sizeOf()-1; i++){
+            noRetorno = noAuxiliar;
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        return noRetorno;
+    }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        No<T> noAuxiliar = referenciaEntrada;
+
+        for (int i = 0; i < this.sizeOf(); i++){
+
+            strRetorno += "No{conteudo=" +
+                    noAuxiliar.getConteudo() + "} ----->";
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        strRetorno += null;
+
+        return strRetorno;
+    }
 }
